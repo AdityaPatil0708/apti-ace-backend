3d4cf560318e8ecb789310c6663665f18c1b2cae
Aditya Patil <aditya.patil@aticanlabs.com>
2026-05-15 17:39:53 +0530

    added cors

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import type { Request, Response } from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 import userRoute from "./routes/userRoute.js"
 
 dotenv.config();
@@ -10,6 +11,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
+
 app.use("/api/v1", userRoute)
 
 app.get("/", (req: Request, res: Response) => {
